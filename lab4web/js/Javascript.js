88c0d23cbef5e00f6aba26620cdbd04d76d1edc8
Marik3451 <106335954+Marik3451@users.noreply.github.com>
2023-11-22 17:44:15 +0200

    update 8-9 lab

--- lab4web/js/Javascript.js
+++ lab4web/js/Javascript.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var animatedElements = document.querySelectorAll('.animated');
 
     function checkAnimation() {
-        animatedElements.forEach(function(element) {
+        animatedElements.forEach(function (element) {
             var position = element.getBoundingClientRect();
             if (position.top < window.innerHeight && position.bottom >= 0) {
                 element.classList.add('animate__animated');
@@ -13,7 +13,6 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', checkAnimation);
     checkAnimation();
 });
-AOS.init();
 
 // Функція для відкриття вспливаючого вікна
 function openLoginModal() {
@@ -53,7 +52,7 @@ function updateModels() {
     // Відповідно до вибраної компанії, додаємо моделі
     if (firmSelect.value === "Volkswagen") {
         addOption(modelSelect, "Tiguan", "Tiguan");
-        addOption(modelSelect,"Golf 7", "Golf 7")
+        addOption(modelSelect, "Golf 7", "Golf 7")
         addOption(modelSelect, "Passat B8", "Passat B8");
         addOption(modelSelect, "Jetta", "Jetta");
         // Додайте інші моделі Volkswagen за необхідності
@@ -88,4 +87,58 @@ function addOption(selectElement, value, text) {
 function redirectToHomePage() {
     // Використовуйте window.location.href для переадресації на головну сторінку
     window.location.href = 'index.html';
+}
+
+function getFormValues() {
+    // Отримуємо значення для чекбоксів
+    var pidigrSidiniaChecked = document.getElementsByName('function')[0].checked;
+    var twoSezonClimatronikChecked = document.getElementsByName('function')[1].checked;
+    var lukChecked = document.getElementsByName('function')[2].checked;
+    var salonAlCantaraChecked = document.getElementsByName('function')[3].checked;
+    var electropaketChecked = document.getElementsByName('function')[4].checked;
+
+    // Отримуємо значення для текстового поля
+    var emailValue = document.getElementsByName('function')[5].value;
+
+    // Отримуємо значення для селектів
+    var firmValue = document.getElementById('Firm').value;
+    var modelValue = document.getElementById('model').value;
+    var fullDriveChecked = document.getElementById('fullDrive').checked;
+    var costMoneyValue = document.getElementById('costMoney').value;
+    var costValue = document.getElementById("cost").value;
+
+    var displayRadioValue = (fullDriveChecked) ? 'Повний привід' : 'Передній привід';
+    // Виводимо значення у консоль для перевірки (можна замінити на інші дії)
+    console.log('Компанія:', firmValue);
+    console.log('Модель:', modelValue);
+    console.log('Тип приводу:', displayRadioValue);
+    console.log('Валюта:', costValue);
+    console.log('Ціна:', costMoneyValue);
+    console.log('Підгрів сидінь:', pidigrSidiniaChecked);
+    console.log('Двухзонний кліматронік:', twoSezonClimatronikChecked);
+    console.log('Люк:', lukChecked);
+    console.log('Салон Аль-Кантара:', salonAlCantaraChecked);
+    console.log('Електропакет:', electropaketChecked);
+    console.log('Email:', emailValue);
+
+    // Створюємо об'єкт для збереження значень
+    var formData = {
+        'Компанія': firmValue,
+        'Модель': modelValue,
+        'Тип приводу': displayRadioValue,
+        'Валюта': costValue,
+        'Ціна': costMoneyValue,
+        'Підгрів сидінь': pidigrSidiniaChecked,
+        'Двухзонний кліматронік': twoSezonClimatronikChecked,
+        'Люк': lukChecked,
+        'Салон Аль-Кантара': salonAlCantaraChecked,
+        'Електропакет': electropaketChecked,
+        'Email': emailValue
+    };
+
+    // Конвертуємо в JSON
+    var jsonData = JSON.stringify(formData);
+
+    // Виводимо JSON-рядок у консоль
+    console.log('JSON-дані:', jsonData);
 }
